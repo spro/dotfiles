b3af9176329c1682d8b02badb04d5c9dbc821139
Sean Robertson <sprobertson@gmail.com>
2025-04-14 17:03:29 -0700

    Update resizing.lua

--- hammerspoon/resizing.lua
+++ hammerspoon/resizing.lua
@@ -1,10 +1,12 @@
+local hs = hs
+
 -- Resizing / moving windows
 --------------------------------------------------------------------------------
 
 hs.window.animationDuration = 0.1
 
-move_amount = 200
-move_more_amount = 500
+local move_amount = 200
+local move_more_amount = 500
 
 function moveLeft(f) f.x = f.x - move_amount end
 function moveRight(f) f.x = f.x + move_amount end
@@ -26,6 +28,16 @@ function moveScreenRight()
     moveFullScreen(win, win:screen():toEast())
 end
 
+function moveScreenUp()
+    local win = hs.window.focusedWindow()
+    moveFullScreen(win, win:screen():toNorth())
+end
+
+function moveScreenDown()
+    local win = hs.window.focusedWindow()
+    moveFullScreen(win, win:screen():toSouth())
+end
+
 function resizeFull(f, win)
     local screen = win:screen()
     local max = screen:frame()
@@ -35,41 +47,76 @@ function resizeFull(f, win)
     f.h = max.h
 end
 
-function resizeHalfLeft(f, win)
+function resizeHorizontal(width, start)
+    return function (f, win)
+        local screen = win:screen()
+        local max = screen:frame()
+        f.x = max.x + max.w * start
+        f.y = max.y
+        f.w = max.w * width
+        f.h = max.h
+    end
+end
+
+function resizeVertical(height, start, keepHeight)
+    return function (f, win)
+        local screen = win:screen()
+        local max = screen:frame()
+        f.y = max.y + max.h * start
+        f.h = max.h * height
+        if not keepHeight then
+            f.x = max.x
+            f.w = max.w
+        end
+    end
+end
+
+-- Corners
+-- ------------
+
+function resizeTopLeft(f, win)
     local screen = win:screen()
     local max = screen:frame()
     f.x = max.x
     f.y = max.y
-    f.w = max.w / 2
-    f.h = max.h
+    f.w = max.w * 0.5
+    f.h = max.h * 0.5
 end
 
-function resizeHalfRight(f, win)
+function resizeTopRight(f, win)
     local screen = win:screen()
     local max = screen:frame()
-    f.x = max.x + max.w / 2
+    f.x = max.x + max.w * 0.5
     f.y = max.y
-    f.w = max.w / 2
-    f.h = max.h
+    f.w = max.w * 0.5
+    f.h = max.h * 0.5
 end
 
-function resizeHalfTop(f, win)
+function resizeBottomLeft(f, win)
     local screen = win:screen()
     local max = screen:frame()
     f.x = max.x
-    f.y = max.y
-    f.w = max.w
-    f.h = max.h / 2
+    f.y = max.y + max.h * 0.5
+    f.w = max.w * 0.5
+    f.h = max.h * 0.5
 end
 
-function resizeHalfBottom(f, win)
+function resizeBottomRight(f, win)
     local screen = win:screen()
     local max = screen:frame()
-    f.x = max.x
-    f.y = max.y + max.h / 2
-    f.w = max.w
-    f.h = max.h / 2
+    f.x = max.x + max.w * 0.5
+    f.y = max.y + max.h * 0.5
+    f.w = max.w * 0.5
+    f.h = max.h * 0.5
 end
+
+-- Halves
+-- ------------
+
+resizeLeftHalf = resizeHorizontal(0.5, 0)
+resizeRightHalf = resizeHorizontal(0.5, 0.5)
+resizeTopHalf = resizeVertical(0.5, 0)
+resizeBottomHalf = resizeVertical(0.5, 0.5)
 
 function resizeLeftToEdge(f, win)
     local screen = win:screen()
@@ -89,6 +136,32 @@ function resizeRight(f) f.w = f.w + move_amount end
 function resizeUp(f) f.h = f.h - move_amount end
 function resizeDown(f) f.h = f.h + move_amount end
 
+function resizeCenter(size)
+    return function (f, win)
+	local screen = win:screen()
+	local max = screen:frame()
+	f.x = max.x + max.w * (1.0 - size) / 2.0
+	f.w = max.w * size
+	f.y = max.y + max.h * (1.0 - size) / 2.0
+	f.h = max.h * size
+    end
+end
+
+function resizeTo1080p()
+    return function (f, win)
+        local screen = win:screen()
+        local max = screen:frame()
+
+        -- Set width and height to 1080p dimensions
+        f.w = 1920
+        f.h = 1080
+
+        -- Center the window on the screen
+        f.x = max.x + (max.w - f.w) / 2
+        f.y = max.y + (max.h - f.h) / 2
+    end
+end
+
 -- Note: This is what actually sets the window frame, above functions only build the frame description
 function adjustWindow(mover)
     doMove = function()
@@ -107,6 +180,8 @@ hs.hotkey.bind({"ctrl", "alt", "cmd"}, "J", adjustWindow(moveDown))
 
 hs.hotkey.bind({"ctrl", "alt", "cmd", "shift"}, "H", moveScreenLeft)
 hs.hotkey.bind({"ctrl", "alt", "cmd", "shift"}, "L", moveScreenRight)
+hs.hotkey.bind({"ctrl", "alt", "cmd", "shift"}, "K", moveScreenUp)
+hs.hotkey.bind({"ctrl", "alt", "cmd", "shift"}, "J", moveScreenDown)
 
 hs.hotkey.bind({"ctrl", "alt"}, "H", adjustWindow(resizeLeft))
 hs.hotkey.bind({"ctrl", "alt"}, "L", adjustWindow(resizeRight))
@@ -114,17 +189,29 @@ hs.hotkey.bind({"ctrl", "alt"}, "K", adjustWindow(resizeUp))
 hs.hotkey.bind({"ctrl", "alt"}, "J", adjustWindow(resizeDown))
 
 hs.hotkey.bind({"ctrl", "cmd"}, "F", adjustWindow(resizeFull))
-hs.hotkey.bind({"ctrl", "cmd"}, "H", adjustWindow(resizeHalfLeft))
-hs.hotkey.bind({"ctrl", "cmd"}, "L", adjustWindow(resizeHalfRight))
-hs.hotkey.bind({"ctrl", "cmd"}, "K", adjustWindow(resizeHalfTop))
-hs.hotkey.bind({"ctrl", "cmd"}, "J", adjustWindow(resizeHalfBottom))
+hs.hotkey.bind({"ctrl", "cmd"}, "H", adjustWindow(resizeLeftHalf))
+hs.hotkey.bind({"ctrl", "cmd"}, "L", adjustWindow(resizeHorizontal(0.5, 0.5)))
+hs.hotkey.bind({"ctrl", "cmd"}, "K", adjustWindow(resizeTopHalf))
+hs.hotkey.bind({"ctrl", "cmd"}, "J", adjustWindow(resizeBottomHalf))
 
-hs.hotkey.bind({"ctrl", "cmd"}, "G", adjustWindow(resizeLeftToEdge))
-hs.hotkey.bind({"ctrl", "cmd"}, ";", adjustWindow(resizeRightToEdge))
+hs.hotkey.bind({"ctrl", "cmd", "shift"}, "K", adjustWindow(resizeVertical(0.5, 0, true)))
+hs.hotkey.bind({"ctrl", "cmd", "shift"}, "J", adjustWindow(resizeVertical(0.5, 0.5, true)))
+
+-- hs.hotkey.bind({"ctrl", "cmd"}, "G", adjustWindow(resizeLeftToEdge))
+-- hs.hotkey.bind({"ctrl", "cmd"}, ";", adjustWindow(resizeRightToEdge))
+hs.hotkey.bind({"ctrl", "cmd"}, "G", adjustWindow(resizeHorizontal(0.333, 0)))
+-- hs.hotkey.bind({"ctrl", "cmd"}, "C", adjustWindow(resizeHorizontal(0.333, 0.333)))
+hs.hotkey.bind({"ctrl", "cmd"}, "C", adjustWindow(resizeCenter(0.5)))
+hs.hotkey.bind({"ctrl", "cmd", "shift"}, "C", adjustWindow(resizeCenter(0.75)))
+hs.hotkey.bind({"ctrl", "cmd"}, "1", adjustWindow(resizeTo1080p()))
+hs.hotkey.bind({"ctrl", "cmd"}, ";", adjustWindow(resizeHorizontal(0.333, 0.666)))
+
+hs.hotkey.bind({"ctrl", "cmd"}, "u", adjustWindow(resizeTopLeft))
+hs.hotkey.bind({"ctrl", "cmd"}, "n", adjustWindow(resizeBottomLeft))
+hs.hotkey.bind({"ctrl", "cmd"}, "o", adjustWindow(resizeTopRight))
+hs.hotkey.bind({"ctrl", "cmd"}, ".", adjustWindow(resizeBottomRight))
 
 hs.hotkey.bind({"alt", "cmd"}, "H", hs.window.filter.focusWest)
 hs.hotkey.bind({"alt", "cmd"}, "L", hs.window.filter.focusEast)
 hs.hotkey.bind({"alt", "cmd"}, "K", hs.window.filter.focusNorth)
 hs.hotkey.bind({"alt", "cmd"}, "J", hs.window.filter.focusSouth)
-
-
