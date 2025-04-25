b443cdadfb3fe0c1e28b0bc44f934e5ed19012df
Sean Robertson <sprobertson@gmail.com>
2025-04-25 12:39:29 -0700

    glucose menubar item

---
+++ hammerspoon/glucose.lua
@@ -0,0 +1,72 @@
+local glucose_url = hs.settings.get("glucose_url")
+
+if glucose_url == nil then
+    hs.notify.new({
+        title = "Glucose menubar error",
+        informativeText = 'use hs.settings.set("glucose_url", "...")',
+    }):send()
+    return
+end
+
+local glucose_menuitem = hs.menubar.new()
+
+local function reverse(tab)
+    for i = 1, #tab // 2, 1 do
+        tab[i], tab[#tab - i + 1] = tab[#tab - i + 1], tab[i]
+    end
+    return tab
+end
+
+function updateGlucose()
+    -- Make request
+    local status, body = hs.http.get(glucose_url)
+    local response = hs.json.decode(body)
+    local latest = response["latest"]
+
+    -- Get min/max values
+    local values = {}
+    local v_min = 500
+    local v_max = 0
+    for i, l in ipairs(latest) do
+        local v = l["value"]
+        if v < v_min then
+            v_min = v
+        end
+        if v > v_max then
+            v_max = v
+        end
+        table.insert(values, v)
+    end
+
+    values = reverse(values)
+
+    -- Set up canvas
+    local a = hs.canvas.new { h = 16, w = 64 }:show()
+    a[1] = {
+        type = "segments",
+        closed = false,
+        action = "stroke",
+        strokeColor = { red = 1, green = 1 },
+        strokeWidth = 2
+    }
+
+    -- Build bar graph
+    local z = {}
+    for x = 1, #values do
+        local v = values[x]
+        table.insert(z, {
+            x = x - 1,
+            y = 16 - (v - v_min) / (v_max - v_min) * 16
+        })
+    end
+    a[1].coordinates = z
+
+    -- Set icon and value
+    glucose_menuitem:setIcon(a:imageFromCanvas())
+    glucose_menuitem:setTitle(values[#values])
+end
+
+hs.timer.doEvery(60 * 5, updateGlucose)
+updateGlucose()
+
+glucose_menuitem:setClickCallback(updateGlucose)
